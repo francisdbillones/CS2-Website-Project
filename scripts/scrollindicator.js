8103dce0a421e0c4d460cdb1066c408aecf72878
francisdbillones <francisd.billones@gmail.com>
2021-01-15 23:48:31 +0800

    scroll indicator finally works on help page

--- scripts/scrollindicator.js
+++ scripts/scrollindicator.js
@@ -1,14 +1,13 @@
 // reflect user's scroll progress as a progress bar
 
-console.log("Starting scrollindicator.js");
-
+console.log("loading scrollindicator.js");
 window.onscroll = function() {scroll_indicator()};
 
-var bar = document.getElementById("bar");
+const bar = document.getElementById("bar");
+
 const clientHeight = document.documentElement.clientHeight;
 
 function scroll_indicator(){
-    console.log("scrolling");
     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     
     let height = document.documentElement.scrollHeight - clientHeight;
