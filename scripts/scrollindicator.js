819c5a82a8c70ad94d44aaa26c41e5ce4a02fa29
francisdbillones <francisd.billones@gmail.com>
2021-01-15 23:19:06 +0800

    added citations for images, scroll indicator in donation page is still not working

--- scripts/scrollindicator.js
+++ scripts/scrollindicator.js
@@ -1,11 +1,14 @@
 // reflect user's scroll progress as a progress bar
 
+console.log("Starting scrollindicator.js");
+
 window.onscroll = function() {scroll_indicator()};
 
-const bar = document.getElementById("bar");
+var bar = document.getElementById("bar");
 const clientHeight = document.documentElement.clientHeight;
 
 function scroll_indicator(){
+    console.log("scrolling");
     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     
     let height = document.documentElement.scrollHeight - clientHeight;
@@ -13,5 +16,6 @@ function scroll_indicator(){
     let scrolled = (winScroll / height) * 100;
     
     bar.style.width = scrolled + "%";
+    console.log(scrolled + "%");
 }
 
